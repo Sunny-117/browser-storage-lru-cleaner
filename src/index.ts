4f762430f362fa82aec429dea3c7b6f928214483
Sunny-117 <zhiqiangfu6@gmail.com>
2025-09-12 22:27:02 +0800

    refactor: 💡 exports

--- src/index.ts
+++ src/index.ts
@@ -13,15 +13,3 @@ export type {
   IAccessRecord,
   IStorageStats
 } from './interfaces';
-
-// 适配器导出
-export { LocalStorageAdapter, IndexedDBAdapter } from './adapters';
-
-// 策略导出
-export { LRUStrategy } from './strategies';
-
-// 工具导出
-export { Utils } from './utils';
-
-// 版本信息
-export const VERSION = '1.0.0';
